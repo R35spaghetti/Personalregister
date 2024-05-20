0e2fb821b254f11d1e1e7a296e8527582902610d
rb <rkrb@tyfonmail.se>
2024-05-20 16:18:26 +0200

    error handling for invalid salary

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -15,7 +15,7 @@ do
     {
         ErrorMessageInvalidChoice();
     }
-    
+
     switch (choice)
     {
         case 1:
@@ -23,10 +23,17 @@ do
             string employeeNameInput = Console.ReadLine() ?? string.Empty;
             Console.WriteLine("Enter employee's salary");
             string employeeSalaryInput = Console.ReadLine() ?? string.Empty;
-            Employee employee = new Employee(employeeNameInput, Convert.ToInt32(employeeSalaryInput));
+            if (int.TryParse(employeeSalaryInput, out _))
+            {
+                Employee employee = new Employee(employeeNameInput, Convert.ToInt32(employeeSalaryInput));
+                employeesList.Add(employee);
+                Console.WriteLine($"Added employee {employee.Name} with salary {employee.Salary} to the list.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid salary input, returning to menu");
+            }
 
-            employeesList.Add(employee);
-            Console.WriteLine($"Added employee {employee.Name} with salary {employee.Salary} to the list.");
             break;
         case 2:
             Console.WriteLine("List of employees:");
@@ -37,12 +44,10 @@ do
         case 3:
             Console.WriteLine("Exits program..");
             break;
-   
     }
 } while (choice != 3);
 
 void ErrorMessageInvalidChoice()
 {
     Console.WriteLine("Invalid choice, please enter a number between 1-3");
-
 }
