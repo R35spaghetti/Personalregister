de7a4561c8bff3a84ffb1ffd8f70aeb39c90f953
rb <rkrb@tyfonmail.se>
2024-05-20 15:35:30 +0200

    Menu with 3 choices add employee list employees exit program

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,14 +1,41 @@
 ﻿using Personalregister.Models;
 
-
-Console.WriteLine("Enter employee's name");
-string employeeNameInput = Console.ReadLine() ?? string.Empty;
-Console.WriteLine("Enter employee's salary");
-string employeeSalaryInput = Console.ReadLine() ?? string.Empty;
-Employee employee = new Employee(employeeNameInput, Convert.ToInt32(employeeSalaryInput));
+int choice;
 List<Employee> employeesList = new List<Employee>();
 
-employeesList.Add(employee);
-Console.WriteLine($"Added employee {employee.Name} with salary {employee.Salary} to the list.");
+do
+{
+    Console.WriteLine("Enter the number to choose \n" +
+                      " 0 - Add employee \n" +
+                      " 1 - Show all employees \n" +
+                      " 2 - Exit program");
+    choice = Convert.ToInt32(Console.ReadLine());
 
+    switch (choice)
+    {
+        case 0:
+            Console.WriteLine("Enter employee's name");
+            string employeeNameInput = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Enter employee's salary");
+            string employeeSalaryInput = Console.ReadLine() ?? string.Empty;
+            Employee employee = new Employee(employeeNameInput, Convert.ToInt32(employeeSalaryInput));
 
+            employeesList.Add(employee);
+            Console.WriteLine($"Added employee {employee.Name} with salary {employee.Salary} to the list.");
+            break;
+        case 1:
+            Console.WriteLine("List of employees:");
+            foreach (var item in employeesList)
+            {
+                Console.WriteLine($"Employee {item.Name} with salary {item.Salary} to the list.");
+            }
+
+            break;
+        case 2:
+            Console.WriteLine("Exits program..");
+            break;
+        default:
+            Console.WriteLine("Invalid choice, enter either 1 or 0.");
+            break;
+    }
+} while (choice != 2);
