eebd2c025f8722075708bf8437ef1a67c3dc7824
rb <rkrb@tyfonmail.se>
2024-05-20 15:15:29 +0200

    name & salary can be added to a list of employees - first iteration

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Personalregister.Models;
+
+
+Console.WriteLine("Enter employee's name");
+string employeeNameInput = Console.ReadLine() ?? string.Empty;
+Console.WriteLine("Enter employee's salary");
+string employeeSalaryInput = Console.ReadLine() ?? string.Empty;
+Employee employee = new Employee(employeeNameInput, Convert.ToInt32(employeeSalaryInput));
+List<Employee> employeesList = new List<Employee>();
+
+employeesList.Add(employee);
+Console.WriteLine($"Added employee {employee.Name} with salary {employee.Salary} to the list.");
+
+
